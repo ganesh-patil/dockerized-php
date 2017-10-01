4eac6ac0ca8cad2f96d36569700e3e2394537647
ganeshp-c <ganeshp@surveymonkey.com>
2017-10-01 22:47:25 +0530

    mysql changes

--- app/index.php
+++ app/index.php
@@ -1,28 +1,29 @@
 <?php 
 try {
  echo "=========Welcome to Ganesh patil's PHP appliction build using docker containers: ===<br>";
-$con = mysqli_connect("dockerizedphp_mysql_1", "root", "root", "TEST", 3306);
+$con = mysqli_connect("mysql", "root", "root","TEST");
 mysqli_query($con,"CREATE TABLE IF NOT EXISTS  emp (emp_id INT NOT NULL AUTO_INCREMENT, name VARCHAR(20), PRIMARY KEY (emp_id)) ");
 //echo mysqli_errno($con) . ": " . mysqli_error($con). "\n";
-mysqli_query($con,"INSERT INTO emp (name)
-VALUES ('Ganesh')");
+mysqli_query($con,"INSERT INTO emp (name) VALUES ('Ganesh')");
 
 //echo mysqli_errno($con) . ": " . mysqli_error($con). "\n";
 $data = mysqli_query($con,"SELECT * FROM TEST.emp");
 
-//echo mysqli_errno($con) . ": " . mysqli_error($con). "\n";
-$data = $data->fetch_all(MYSQLI_ASSOC);
-//var_export($data);
-if(!empty($data)) {
+ while($row = $data->fetch_assoc())
+        {
+            $rows[] = $row;
+        }
+
+
+if(!empty($rows)) {
     echo "Following records are present in Database: <br>";
-    foreach($data as $record) {
+    foreach($rows as $record) {
         echo "Record : ID =".$record['emp_id']."  Name :".$record['name']. "<br>";
     }
 }
 mysqli_close($con);
 }
 catch (Exception $e) {
-    
     prin_r($e->getMessage());
 }
 
